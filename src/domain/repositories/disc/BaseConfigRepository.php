3befc1bbfa58903f055b1675e81aeb736cc91af6
Vitaliy <theyamshikov@yandex.ru>
2019-02-04 23:13:08 +0600

    refacto Store

--- src/domain/repositories/disc/BaseConfigRepository.php
+++ src/domain/repositories/disc/BaseConfigRepository.php
@@ -4,7 +4,7 @@ namespace yii2rails\app\domain\repositories\disc;
 
 use yii2rails\domain\BaseEntity;
 use yii2rails\domain\repositories\BaseRepository as YiiBaseRepository;
-use yii2rails\extension\store\Store;
+use yii2rails\extension\store\StoreFile;
 
 class BaseConfigRepository extends YiiBaseRepository {
 
@@ -13,18 +13,18 @@ class BaseConfigRepository extends YiiBaseRepository {
 
 	public function load() {
 		$store = $this->storeInstance();
-		$config = $store->load($this->file, $this->key);
+		$config = $store->load($this->key);
 		return $this->forgeEntity($config);
 	}
 	
 	public function save(BaseEntity $entity) {
 		$entity->validate();
 		$store = $this->storeInstance();
-		$store->update($this->file, $this->key, $entity->toArray());
+		$store->update($this->key, $entity->toArray());
 	}
 
 	protected function storeInstance() {
-		$store = new Store('php');
+		$store = new StoreFile($this->file);
 		return $store;
 	}
 
