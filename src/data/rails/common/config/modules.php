1cc95cc7a8a62a610904fa08dc7f7330a4d854f7
Vitaliy <theyamshikov@yandex.ru>
2019-02-15 23:05:59 +0600

    refacto config

--- src/data/rails/common/config/modules.php
+++ src/data/rails/common/config/modules.php
@@ -1,22 +1,9 @@
 <?php
 
-use yii2lab\applicationTemplate\common\enums\ApplicationPermissionEnum;
-use yii2rails\extension\web\helpers\Behavior;
+use yii2lab\applicationTemplate\common\helpers\CommonModuleHelper;
 
 return [
-	'lang' => [
-		'class' => 'yii2module\lang\module\Module',
-	],
-	'debug' => [
-		'class' => 'yii\debug\Module',
-		'allowedIPs' => ['127.0.0.1'],
-		'as access' => Behavior::access(ApplicationPermissionEnum::BACKEND_ALL),
-	],
-	'gii' => [
-		'class' => 'yii\gii\Module',
-		'allowedIPs' => ['127.0.0.1'],
-		'as access' => Behavior::access(ApplicationPermissionEnum::BACKEND_ALL),
-		'isEnabled' => YII_ENV_DEV,
-	],
+	'lang' => 'yii2module\lang\module\Module',
+	'debug' => CommonModuleHelper::getConfig('debug'),
+	'gii' => CommonModuleHelper::getConfig('gii'),
 ];
-
