4f60b4bc0e4b25b262bba4d9840b69813ff95a7e
Vitaliy <theyamshikov@yandex.ru>
2019-03-12 00:35:50 +0600

    make load env-test

--- src/domain/filters/env/LoadConfig.php
+++ src/domain/filters/env/LoadConfig.php
@@ -3,19 +3,25 @@
 namespace yii2rails\app\domain\filters\env;
 
 use yii\helpers\ArrayHelper;
+use yii2rails\app\domain\enums\YiiEnvEnum;
 use yii2rails\extension\scenario\base\BaseScenario;
 
 class LoadConfig extends BaseScenario {
 
 	const FILE_ENV = 'env';
 	const FILE_ENV_LOCAL = 'env-local';
+    const FILE_ENV_TEST = 'env-test';
 	const FILE_ENV_SYSTEM_LOCAL = 'env-system-local';
 	
 	public $paths = [];
 	
 	public function run() {
 		$mainConfig = $this->loadByFileName(self::FILE_ENV);
-		$localConfig = $this->loadByFileName(self::FILE_ENV_LOCAL);
+        $localConfig = $this->loadByFileName(self::FILE_ENV_LOCAL);
+		if(defined('YII_ENV') && YII_ENV == 'test') {
+            $testConfig = $this->loadByFileName(self::FILE_ENV_TEST);
+            $localConfig = ArrayHelper::merge($localConfig, $testConfig);
+        }
 		$systemLocalConfig = $this->loadByFileName(self::FILE_ENV_SYSTEM_LOCAL);
 		$config = ArrayHelper::merge($mainConfig, $localConfig);
 		$config = ArrayHelper::merge($config, $systemLocalConfig);
