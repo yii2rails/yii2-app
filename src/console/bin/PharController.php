be59b3ad8af70dd4e43c54e393169f84a7ab7adf
Vitaliy <theyamshikov@yandex.ru>
2019-06-16 21:02:55 +0600

    feat: make php cache

--- src/console/bin/PharController.php
+++ src/console/bin/PharController.php
@@ -5,6 +5,7 @@ namespace yii2rails\app\console\bin;
 use Yii;
 use yii\helpers\ArrayHelper;
 use yii2rails\app\domain\helpers\phar\PharCacheHelper;
+use yii2rails\extension\code\helpers\CodeCacheHelper;
 use yii2rails\extension\console\handlers\RenderHahdler;
 use yii2rails\extension\console\helpers\input\Select;
 use yii2rails\extension\console\helpers\Output;
@@ -24,6 +25,28 @@ class PharController extends \yii\base\Component
         Output::line();
 	}
 
+    public function actionMakeCache2()
+    {
+        Output::line('Update cache...');
+
+        $select = Select::display('From', ['all', 'common']);
+        $from = Select::getFirstValue($select);
+        if($from == 'all') {
+            $all = \App::$domain->tool->runtimeInclude->allUnique();
+        } elseif ($from == 'common') {
+            $all = PharCacheHelper::allCommon();
+        }
+
+        CodeCacheHelper::saveClassesCache($all);
+
+        Output::block('Cache updated!');
+        $size = filesize(CodeCacheHelper::getFileName());
+        Output::arr([
+            'total files' => count($all),
+            'size' => FileHelper::sizeFormat($size),
+        ]);
+    }
+
 	public function actionMakeCache()
 	{
 		Output::line('Update cache...');
@@ -36,10 +59,12 @@ class PharController extends \yii\base\Component
             $all = PharCacheHelper::allCommon();
         }
 
-        //d([count($all), $all]);
+        d([count($all), $all]);
 
         PharCacheHelper::setRendeClass(RenderHahdler::class);
         PharCacheHelper::addClassListWithParents($all);
+
+
         Output::block('Cache updated!');
         $size = filesize(ROOT_DIR . DS . PHAR_FILE);
         Output::arr([
