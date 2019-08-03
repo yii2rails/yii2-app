fb257b7fe01716f1b1bc8a4da53defa17fd1b515
Vitaliy <theyamshikov@yandex.ru>
2019-08-03 16:35:38 +0600

    refacto: refacto account namespace

--- src/php/config/main.php
+++ src/php/config/main.php
@@ -8,7 +8,7 @@ return [
     'components' => [
         'language' => 'yii2bundle\lang\domain\components\Language',
         'user' => [
-            'class' => 'yii2module\account\domain\v3\web\User',
+            'class' => 'yii2bundle\account\domain\v3\web\User',
         ],
         'authManager' => 'yii2bundle\rbac\domain\rbac\PhpManager',
         'cache' => [
